97cec4cd8f7df8031dd9d1ba1bbb358626991be7
Steve LeValley <slevalley@gmail.com>
2021-04-19 22:34:47 -0700

    Need to Create Functions for all options

--- schema.sql
+++ schema.sql
@@ -4,23 +4,22 @@ create database if not exists employees_DB;
 use employees_db;
 
 CREATE TABLE IF NOT EXISTS department (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NOT NULL
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS role (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(12, 3) NOT NULL,
+    salary DECIMAL(7) NOT NULL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS employee (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    department_id INT, 
+    role_id INT NOT NULL, 
     manager_id INT,
-    FOREIGN KEY(department_id) REFERENCES department(id)
-    
+    FOREIGN KEY(role_id) REFERENCES role(id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,14 @@
+use employees_db;
+
 INSERT INTO department (name)
 VALUES ('Human Resources'), ('Accounting'), ('Engineering');
 
+INSERT INTO role (title, salary, department_id)
+VALUES ('HR Manager', 90000, 1), ('HR Employee', 75000, 1), 
+       ('Accounting Manager', 105000, 2), ('Accountant', 92000, 2),
+       ('Engineering Manager', 125000, 3), ('Engineer', 105000, 3);
 
-
-SELECT * FROM department;
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Joe', 'Rogan', 1, NULL), ('Megan', 'Fox', 2, 1), 
+	   ('Taylor', 'Swift', 3, NULL), ('Betty', 'White', 4, 3), 
+       ('Snoop', 'Dogg', 5, NULL), ('Martha', 'Stewart', 6, 5);

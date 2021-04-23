d825ed10640101f696f84bd923c4ad7bfbd27748
Steve LeValley <slevalley@gmail.com>
2021-04-22 17:13:38 -0700

    Separated Functions into Different Files

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ require("console.table");
 const connection = require('./db/connection');
 const { viewAllDepartments, viewAllRoles, viewAllEmployees, viewEmployeesByDepartment, viewEmployeesByRole } = require('./functions/viewFunctions');
 
-
-
-
 connection.connect((err) => {
   if (err) throw err;
   initialPrompt();
@@ -79,91 +76,6 @@ const initialPrompt = () => {
       }
     });
 };
-
-
-
-
-// // ########################### View Employees By Department #######################
-// // ########################### View Employees By Department #######################
-// // ########################### View Employees By Department #######################
-
-
-// const viewEmployeesByDepartment = () => {
-//   inquirer
-//     .prompt({
-//       name: 'department',
-//       type: 'rawlist',
-//       message: 'Which department would you like to view?',
-//       choices: [
-//         'Human Resources',
-//         'Accounting',
-//         'Engineering',
-//       ],
-//     }).then((answer) => {
-
-//       if (answer.department === 'Human Resources') {
-//         answer.department = 1;
-//       } else if (answer.department === 'Accounting') {
-//         answer.department = 2;
-//       } else {
-//         answer.department = 3;
-//       }
-//       let query = 'SELECT employee_id AS "Employee ID", first_name AS "First Name", last_name AS "Last Name", ';
-//       query += 'employee.department_id AS "Department ID" FROM employee ';
-//       query += `INNER JOIN department ON employee.department_id =  department.department_id where department.department_id = ${answer.department};`;
-//       connection.query(query, (err, res) => {
-//         console.table(res);
-//         console.table('-----------------------------------');
-//         connection.end();
-//       })
-//     })
-// };
-
-// ########################### View Employees By Role ##############################
-// ########################### View Employees By Role ##############################
-// ########################### View Employees By Role ##############################
-
-// const viewEmployeesByRole = () => {
-//   inquirer
-//     .prompt({
-//       name: 'role',
-//       type: 'rawlist',
-//       message: 'Which role would you like to view?',
-//       choices: [
-//         'Human Resources Manager',
-//         'Human Resources Employee',
-//         'Accounting Manager',
-//         'Accountant',
-//         'Engineering Manager',
-//         'Engineer',
-//       ],
-//     }).then((answer) => {
-
-//       if (answer.role === 'Human Resources Manager') {
-//         answer.role = 1;
-//       } else if (answer.role === 'Human Resources Employee') {
-//         answer.role = 2;
-//       } else if (answer.role === 'Accounting Manager') {
-//         answer.role = 3;
-//       } else if (answer.role === 'Accountant') {
-//         answer.role = 4;
-//       } else if (answer.role === 'Engineering Manager') {
-//         answer.role = 5;
-//       } else {
-//         answer.role = 6;
-//       }
-
-//       let query = 'SELECT employee_id AS "Employee ID", first_name AS "First Name", last_name AS "Last Name", ';
-//       query += 'role.title AS "Position", role.salary AS "Salary" FROM employee ';
-//       query += `INNER JOIN role ON employee.role_id = role.role_id WHERE role.role_id = ${answer.role}`,
-
-//         connection.query(query, (err, res) => {
-//           console.table(res);
-//           console.table('-----------------------------------');
-//           connection.end();
-//         })
-//     })
-// };
 
 // ############################## Add An Employee ################################
 // ############################## Add An Employee ################################
